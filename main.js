7e49029c3e7783850182be256ca199cb3523a65b
NicksWorld <nickmcdaniel00@gmail.com>
2017-11-05 06:56:42 -0500

    Make window open

---
+++ main.js
@@ -0,0 +1,65 @@
+//Setup variables for use with node.js packages.
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+//Initiate required variables from electron.
+const {app, BrowserWindow, Menu} = electron;
+
+//Initiate the game window.
+let GameWindow;
+
+//Initalize the game windows menu bar.
+const gameMenuTemplate = [
+  //Initialize the menu item 'File'
+  {
+    label: 'File',
+    submenu: [
+      //Initialize the quit item
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.exit();
+        }
+      }
+    ]
+  }
+];
+
+
+//Startup of the application.
+app.on('ready', function(){
+  //Make the window.
+  GameWindow = new BrowserWindow({})
+
+  //Load in the HTML file
+  GameWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'gameWindow.htm'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  //Setup menu for darwin
+  if (process.platform === 'darwin') {
+  gameMenuTemplate.unshift({
+    label: app.getName(),
+    submenu: [
+      {role: 'about'},
+      {type: 'separator'},
+      {role: 'services', submenu: []},
+      {type: 'separator'},
+      {role: 'hide'},
+      {role: 'hideothers'},
+      {role: 'unhide'},
+      {type: 'separator'},
+      {role: 'quit'}
+    ]
+  })
+}
+
+//Setup menu
+const gameMenu = Menu.buildFromTemplate(gameMenuTemplate);
+Menu.setApplicationMenu(gameMenu);
+
+})
