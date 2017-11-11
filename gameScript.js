454b8f94dc9e7c9fbbc268105a79f40e1ac3be78
NicksWorld <nickmcdaniel00@gmail.com>
2017-11-11 09:08:39 -0500

    Update

--- gameScript.js
+++ gameScript.js
@@ -16,6 +16,7 @@ var levelTiles;
 var levelObstacles;
 var levelMisc;
 var levelInteractions;
+var levelItems;
 
 var speed = 1;
 var tileSize = 50;
@@ -30,6 +31,7 @@ function setup() {
   levelObstacles = getLevel1Obstacles();
   levelMisc = getLevel1Misc();
   levelInteractions = getLevel1Interactions();
+  levelItems = getLevel1Items();
 
   var KEYS = [];
    document.onkeydown = function(e) {
@@ -45,6 +47,7 @@ function setup() {
     drawObstacles(levelObstacles);
     drawMisc(levelMisc);
     drawInteractions(levelInteractions);
+    drawItems(levelItems);
     context.drawImage(playerFront, 500, 300, 50, 100);
   }, 10);
 
@@ -137,7 +140,7 @@ function drawObstacles(level){
         var obstacle = row[i2];
         var obstacleImage;
         if(obstacle==1){
-          obstacleImage = brick;
+          obstacleImage = wall;
         }
         if(obstacle!=0){
         context.drawImage(obstacleImage, drawX, drawY, tileSize, tileSize);
@@ -160,10 +163,16 @@ function drawMisc(level){
         var misc = row[i2];
         var miscImage;
         if(misc==1){
-          miscImage = controls_1;
+          miscImage = controls_left;
         }
         if(misc==2){
-          miscImage = controls_2;
+          miscImage = controls_down;
+        }
+        if(misc==3){
+          miscImage = controls_right;
+        }
+        if(misc==4){
+          miscImage = controls_up;
         }
         if(misc!=0){
         context.drawImage(miscImage, drawX, drawY, tileSize, tileSize);
@@ -186,10 +195,30 @@ function drawInteractions(level){
         var interaction = row[i2];
         var interactionImage;
         if(interaction==1){
-          interactionImage = machine;
+          interactionImage = machine_broken;
         }
         if(interaction!=0){
         context.drawImage(interactionImage, drawX, drawY, tileSize, tileSize);
+        }
+
+        drawX += tileSize;
+      }
+    drawX = 500 - (playerX*50);
+    drawY += tileSize;
+  }
+}
+
+function drawItems(level){
+  var drawX = 500 - (playerX*50);
+  var drawY = 300 - (playerY*50) + 50;
+
+  for(var i=0;level.length>i;i++){
+    var row = level[i];
+      for(var i2=0;row.length>i2;i2++){
+        var item = row[i2];
+        var itemImage;
+        if(item!=0){
+        context.drawImage(itemImage, drawX, drawY, tileSize, tileSize);
         }
 
         drawX += tileSize;
--- images/resourceLoader.js
+++ images/resourceLoader.js
@@ -3,31 +3,22 @@ var grass = new Image();
 var dirt = new Image();
 var sand = new Image();
 var playerFront = new Image();
-var brick = new Image();
-var controls_1 = new Image();
-var controls_2 = new Image();
-var machine = new Image();
+var wall = new Image();
+var controls_up = new Image();
+var controls_down = new Image();
+var controls_left = new Image();
+var controls_right = new Image();
+var machine_broken = new Image();
 
-grass.onload = function () {
-    dirt.onload = function () {
-        sand.onload = function() {
-          playerFront.onload = function() {
-            brick.onload = function() {
-              controls_1.onload = function() {
-                controls_2.onload = function() {
-                  machine.onload = setup.bind(this);
-                  machine.src = "images/tiles/machine.png";
-                }
-                controls_2.src = "images/tiles/controls_2.png";
-              }
-              controls_1.src = "images/tiles/controls_1.png";
-            }
-            brick.src = "images/tiles/brick.png";
-          }
-          playerFront.src = "images/game/player_front.png";
-        }
-        sand.src = "images/tiles/sand.png";
-    }
-    dirt.src = "images/tiles/dirt.png";
-}
+
+controls_right.src = "images/tiles/right_arrow.png";
+controls_left.src = "images/tiles/left_arrow.png";
+machine_broken.src = "images/tiles/machine.png";
+controls_up.src = "images/tiles/up_arrow.png";
+controls_down.src = "images/tiles/down_arrow.png";
+wall.src = "images/tiles/wall.png";
+playerFront.src = "images/game/player_front.png";
+sand.src = "images/tiles/sand.png";
+dirt.src = "images/tiles/dirt.png";
 grass.src = "images/tiles/grass.png";
+grass.onload = setup.bind(this);
